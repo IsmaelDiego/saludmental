1d0d863ca1ff87d3e83e84fe0a891cb0276fe390
Ismael Diego <ismaeldiegoqm@gmail.com>
2024-11-15 08:18:35 -0500

    update query

--- views/personal/index.php
+++ views/personal/index.php
@@ -24,12 +24,12 @@ if (!isset($_SESSION['S_IDUSUARIO'])) { //si existe
                     <div class="row">
                         <div class="col-12">
                             <div class="page-title-box d-flex align-items-center justify-content-between">
-                                <h4 class="mb-0 font-size-18">Datatables</h4>
+                                <h4 class="mb-0 font-size-18">FICHA PERSONAL</h4>
 
                                 <div class="page-title-right">
                                     <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
-                                        <li class="breadcrumb-item active">Datatables</li>
+                                        <li class="breadcrumb-item"><a href="javascript: void(0);">DASHBOARD</a></li>
+                                        <li class="breadcrumb-item active">FICHA PERSONAL</li>
                                     </ol>
                                 </div>
                                 
@@ -67,7 +67,20 @@ if (!isset($_SESSION['S_IDUSUARIO'])) { //si existe
                                                 <th>Fecha de Baja</th>
                                             </tr>
                                         </thead>
-                                    
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
+                                    <td>1111</td>
                                     
                                         <tbody>
                                             
